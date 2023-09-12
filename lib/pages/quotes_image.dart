21f0d178a14d22f097d9df1a238753796dbc19c6
nittoranjansarkertanmoy02 <nittaranjansarkertanmoy2022@gmail.com>
2023-09-12 15:09:04 +0600

    day 3

--- lib/pages/quotes_image.dart
+++ lib/pages/quotes_image.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:quoteslife/screens/drawer_screen.dart';
+import 'package:http/http.dart' as http;
+import 'package:cached_network_image/cached_network_image.dart';
 
 class QuotesImage extends StatefulWidget {
   const QuotesImage({super.key});
@@ -9,6 +13,32 @@ class QuotesImage extends StatefulWidget {
 }
 
 class _QuotesImageState extends State<QuotesImage> {
+  String imageUrl = '';
+
+  @override
+  void initState() {
+    super.initState();
+    getImageData();
+  }
+
+  Future<void> getImageData() async {
+    final response =
+        await http.get(Uri.parse('https://zenquotes.io/api/image'));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final imageUrl = data['h'];
+
+      print('Image URL: $imageUrl'); // Print the image URL for debugging
+
+      setState(() {
+        this.imageUrl = imageUrl;
+      });
+    } else {
+      print('Error: ${response.statusCode}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,6 +52,20 @@ class _QuotesImageState extends State<QuotesImage> {
         ),
       ),
       drawer: const DrawerScreen(),
+      body: Center(
+        child: imageUrl.isNotEmpty
+            ? CachedNetworkImage(
+                imageUrl: imageUrl,
+                placeholder: (context, url) => CircularProgressIndicator(),
+                errorWidget: (context, url, error) => Icon(Icons.error),
+              )
+            : CircularProgressIndicator(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImageData,
+        tooltip: 'Fetch Random Image',
+        child: Icon(Icons.refresh),
+      ),
     );
   }
 }
